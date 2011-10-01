96165ab4837eefb9d0906b9bae1b1e39f2665308
tenolan <tenolan@gmail.com>
2011-10-02 00:29:32 +0200

    Module reqfiles

--- etc/puppet/manifests/site.pp
+++ etc/puppet/manifests/site.pp
@@ -1,7 +1,7 @@
 # site.pp
-#import 'nodes.pp'
+import 'nodes.pp'
 $puppetserver = 'pps01.n4.local'
-filebucket { main: server => "pps01.n4.local" }
+#filebucket { main: server => "pps01.n4.local" }
 # defaults
-File { backup => main }
-Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin" }
+#File { backup => main }
+#Exec { path => "/usr/bin:/usr/sbin/:/bin:/sbin" }
